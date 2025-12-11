47c81c2789dc7deb10391b1ca22a0e518230cdd3
Jude Metcalf <209973512+judemetcalf111@users.noreply.github.com>
2025-12-11 14:23:55 +0800

    Added Comments to Infidelity.py

--- src/Infidelity.py
+++ src/Infidelity.py
@@ -1,10 +1,16 @@
 def get_Infidelity(qubit_index, gate_name):
     """
-    Returns the angle for the RX error gate.
+    Returns the Infidelity for specified gates
     Logic: 
-    - Faster gates (R) might have small over-rotations.
-    - Two-qubit gates (CX) usually induce larger errors on both qubits.
-    - specific qubits (e.g., q[4]) might be 'bad' qubits with higher error.
+    - Clearer gates might have small bitflip error rates. (R_Z should have zero!)
+    - Two-qubit gates (CZ) usually induce larger bitflip rates on both qubits. 
+        I have encoded this as one rate for the controller qubit, and another for the controlled.
+    - Specific qubits (e.g., q[4] here) might be bad qubits with high error.
+
+
+    NOTE:
+    This function should be replaced with a matrix, or dictionary, where we access very specific information for each qubit 
+    and it's corresponding gate. For now, if-then statements will do!
     """
     
     # Base error rates
@@ -14,15 +20,15 @@ def get_Infidelity(qubit_index, gate_name):
     elif gate_name == 'cz1':
         base_error = 0.1  # Larger error for entangling gates
     elif gate_name == 'r':
-        base_error = 0.1  # Smaller error for single rotations
+        base_error = 0.1  # Smaller error for single gates
     elif gate_name == 'rz':
-        base_error = 0 # Z-rotations are usually virtual and precise
+        base_error = 0 # Z-rotations should be virtual and very precise
     else:
         print(gate_name + ": Not a registered native gate...")
-        return 0.0 # No error for other gates, flag if outside native gate set
+        return 0.0 # No error for other gates, flag and print if outside native gate set
 
     # Qubit specific multiplier (e.g., Qubit 4 is noisy)
-    ## Make a matrix when have data
+    ## Shouldn't be here when this is rigorous as a dict or a matrix
     qubit_multiplier = 1.5 if qubit_index == 4 else 1.0
     
     return base_error * qubit_multiplier
