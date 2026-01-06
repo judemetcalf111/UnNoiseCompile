a3b81fa3e6974e0e2c477836ee753e0c7f96fb80
Jude Metcalf <209973512+judemetcalf111@users.noreply.github.com>
2026-01-06 15:51:17 +0800

    🍙 Folded the aws get_braket_calibration_dict() function from awsproperties.py -> splitmeasfilter.py

--- src/splitmeasfilter.py
+++ src/splitmeasfilter.py
@@ -13,6 +13,7 @@ import numpy.linalg as nl
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from aquarel import load_theme
+from braket.aws import AwsDevice
 
 # Optional: Apply theme if available
 try:
@@ -24,6 +25,49 @@ except:
 
 width = 6.72 
 height = 4.15 
+
+def get_braket_calibration_dict(device_arn, n_qubits=None):
+    """
+    Returns the params list in the exact format required by MeasFilter.
+    """
+    device = AwsDevice(device_arn)
+    properties = device.properties
+    standardized = getattr(properties, "standardized", None)
+    if standardized and hasattr(standardized, "dict"):
+        qubit_props = standardized.dict()
+    else:
+        try:
+            qubit_props = properties.dict()
+        except:
+            qubit_props = {}
+    
+    one_props = qubit_props["oneQubitProperties"]
+    two_props = qubit_props["twoQubitProperties"]
+    one_props_len = len(one_props)
+
+    if n_qubits is None:
+        n_qubits = one_props_len
+    else:
+        if one_props_len != n_qubits:
+            print(f"Warning: Provided n_qubits={n_qubits} does not match device oneQubitProperties length={one_props_len}. Using device length, as qubits are likely down.")
+        else:
+            n_qubits = one_props_len
+            
+    properties = {}
+    for q in range(n_qubits):
+        q_one_prop = one_props[str(q)]["oneQubitFidelity"]
+        q_two_props = {k: v["twoQubitGateFidelity"][0]["fidelity"]
+                        for k, v in two_props.items() if str(q) in k.split("-")}
+
+        one_gate_fidelity = q_one_prop[0]["fidelity"]
+        pm1p0 = q_one_prop[1]["fidelity"]
+        pm0p1 = q_one_prop[2]["fidelity"]
+        properties[str(q)] = {"pm1p0": pm1p0, "pm0p1": pm0p1, "oneQubitFidelity": one_gate_fidelity, "twoQubitProperties": q_two_props}
+
+    return properties
+
+
+
 
 # --- Helper Functions ---
 
@@ -404,7 +448,7 @@ class SplitMeasFilter:
           Not None after execute inference()
           
     """
-    def __init__(self, qubit_order, home_dir=None, data=None, file_address='data/'):
+    def __init__(self, qubit_order, home_dir=None, data=None, file_address='data/', device=None):
         if home_dir:
             self.home_dir = home_dir
             try:
@@ -418,7 +462,7 @@ class SplitMeasFilter:
         self.prior = {}
         self.post = {}
         self.post_marginals = {f'Qubit{q}': {'0': np.array([]), '1': np.array([])} for q in qubit_order}
-        self.params = None
+        self.params = None if device is None else get_braket_calibration_dict(device, n_qubits=len(qubit_order))
         
         self.data = {'0': np.array([]), '1': np.array([])}
         if data is not None:
