75f8812cee54b720c89b352bfd91d94555c44a1b
Jude Metcalf <209973512+judemetcalf111@users.noreply.github.com>
2026-01-24 15:59:47 +0800

    📀 transitioned main branch requirements and files to focus on error measurement and mitigation, other peripheries moved to ExpandedErrorWorking branch

--- src/splitgatefilter.py
+++ src/splitgatefilter.py
@@ -14,7 +14,6 @@ import scipy.stats as ss
 import numpy.linalg as nl
 from collections import defaultdict
 # For optimization
-from cvxopt import matrix, solvers
 
 import src.splitmeasfilter
 import importlib
